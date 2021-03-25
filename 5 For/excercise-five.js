3e2916b6f68b6b3f6476cc087c17b30a9436de46
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-03-25 10:15:53 -0300

    Excersice 5 solved in JS file

---
+++ 5 For/excercise-five.js
@@ -0,0 +1,46 @@
+/********************************
+a. Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for
+de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+*********************************/
+var myCats = ["artemis", "dexter", "dylan", "frodo", "cleo"];
+for (var i = 0; i < myCats.length; i++) {
+    alert(myCats[i]);
+}
+
+/********************************
+b. Al array anterior convertir la primera letra de cada palabra en mayúscula y
+mostrar una alerta por cada palabra modificada.
+*********************************/
+var myCats = ["artemis", "dexter", "dylan", "frodo", "cleo"];
+for (var i = 0; i < myCats.length; i++) {
+    var firstLetter = myCats[i].substring(0,1);
+    var upper = firstLetter.toUpperCase();
+    var lower = myCats[i].substring(1);
+    var myCat = upper + lower;
+    alert(myCat);
+}
+
+/********************************
+c. Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a)
+recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence.
+Al final mostrar una única alerta con la cadena completa.
+*********************************/
+var myCats = ["artemis", "dexter", "dylan", "frodo", "cleo"];
+var sentence = "";
+for (var i in myCats) {
+    var sentence = sentence + myCats[i] + " ";
+}
+alert(sentence);
+
+/********************************
+d. Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición,
+es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array,
+desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final
+(utilizar console.log).
+*********************************/
+
+var myArray = [];
+for (var i = 0; i < 10; i++) {
+    myArray.push(i);
+}
+console.log(myArray);
