c8ba8b3e0c06155a003c2154fe9437388733d613
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-03-25 12:24:29 -0300

    HTML file deleted and JS file linked to the basic index.html file

--- 2 Strings/excercise-two.js
+++ 2 Strings/excercise-two.js
@@ -41,7 +41,7 @@ var firstWhiteSpace = abrahamQuote.indexOf(" ");
 console.log(firstWhiteSpace);
 
 /********************************
-Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
+f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
 Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de
 ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase,
 toLowerCase y el operador +).
