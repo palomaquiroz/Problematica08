78cff40796ac48443db763a191d5ec91c0842422
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-03-24 23:53:24 -0300

    Excercises a, b and c solved in JS file

---
+++ 1 Variables and Operators/excersice-one.js
@@ -0,0 +1,28 @@
+/********************************
+a. Crear dos variables numéricas y utilizar
+el operador suma para guardar el valor
+de la suma de ambos números en una 3er variable.
+*********************************/
+var firstNumber = 20;
+var secondNumber = 30;
+var sum = firstNumber + secondNumber;
+console.log(sum);
+
+/*********************************
+b. Crear dos variables de tipo String y concatenarlas
+guardando el resultado en una 3er variable.
+**********************************/
+var myFirstName = "Paloma";
+var myLastName = "Quiroz";
+var myName = myFirstName + " " + myLastName;
+console.log(myName);
+
+/*********************************
+c. Crear dos variables de tipo String y sumar
+el largo de cada variable (cantidad de letras del string)
+guardando el resultado la suma una 3er variable (utilizar length).
+**********************************/
+var myOlderCatName = "Artemis";
+var myYoungestCatName = "Dexter";
+var myCatsNamesLength = myOlderCatName.length + myYoungestCatName.length;
+console.log(myCatsNamesLength);
