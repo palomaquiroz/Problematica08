cf0ef779f74d5f465b5d5dd4feb28b6582003154
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-03-25 12:22:34 -0300

    HTML file deleted and JS file linked to the basic index.html file

--- 1 Variables and Operators/excersice-one.js
+++ 1 Variables and Operators/excersice-one.js
@@ -1,7 +1,6 @@
 /********************************
-a. Crear dos variables numéricas y utilizar
-el operador suma para guardar el valor
-de la suma de ambos números en una 3er variable.
+a. Crear dos variables numéricas y utilizar el operador suma para
+guardar el valor de la suma de ambos números en una 3er variable.
 *********************************/
 var firstNumber = 20;
 var secondNumber = 30;
@@ -18,8 +17,7 @@ var myName = myFirstName + " " + myLastName;
 console.log(myName);
 
 /*********************************
-c. Crear dos variables de tipo String y sumar
-el largo de cada variable (cantidad de letras del string)
+c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
 guardando el resultado la suma una 3er variable (utilizar length).
 **********************************/
 var myOlderCatName = "Artemis";
